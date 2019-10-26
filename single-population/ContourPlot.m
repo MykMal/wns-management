88c73c1438e31a410bba4ff9b606eb69a65ee708
MykMal <developermisha@gmail.com>
2019-10-26 16:31:29 +0200

    rename folders

---
+++ single-population/ContourPlot.m
@@ -0,0 +1,96 @@
+% MATLAB code for J Duan, MM Malakhov, JJ Pellett, IS Phadke, J Barber, JC Blackwood. "Management Efficacy in a Spatially Dynamic Model of White-nose Syndrome"
+
+% Run this program file to make a heat map of population survival with varying phi, beta for the single-population case.
+% This program file also generates ContourLine.mat, which contains the 25% survival contour values.
+
+% Instructions: Set the desired number of years in MainWNS.m before running.
+% Nothing needs to be modified here.
+
+% Dependencies: This program requires MainWNS.m to be in the same directory.
+
+% version 07/27/2019
+% Copyright (c) 2019 Mykhaylo M. Malakhov
+
+clear all
+
+% create a new figure so other open figures don't get overwritten, and do
+% not show it because it will be saved automatically
+figure('visible','off')
+
+n = 100; % how many values of beta and phi to iterate through; 100 is what we used
+
+controlIntensities = [0 0 0 0 0]; % keep this at 0 when generating ContourLine.mat
+
+outputXYAxes = zeros(n+1,2); % the zeros will be replaced with beta (x-axis) and phi (y-axis)
+contourMatrix = zeros(n+1,n+1); % the output matrix that will be fed into contour
+
+betaMAX = 2 * 10^(-5); % max value of beta to iterate through
+phiMAX = 0.8 * 10^(-12); % max value of phi to iterate through
+
+% the two iterators (iteratorY and iteratorX) are for keeping track of what for loop iteration we're currently in
+iteratorY = 1; % phi loop iterations
+
+% loop through all phi values
+for phi = 0:(phiMAX/n):phiMAX
+    
+    iteratorX = 1; % beta loop iterations
+
+	% loop through all beta values
+    for beta = 0:(betaMAX/n):betaMAX
+        
+        % MainWNS returns [fullTimeVector,populationOutput] so the ~ simply avoids accepting the time vector
+        [~,percentSurvival] = MainWNS(beta,phi,controlIntensities);
+        
+        % here x and y need to be reversed; it is equivalent to transposing
+        % contourMatrix, which is necessary because of how contour works
+        contourMatrix(iteratorY,iteratorX) = percentSurvival;
+        
+        outputXYAxes(iteratorX,1) = beta; % sets first column to be the beta values
+        iteratorX = iteratorX + 1;
+        
+    end % ends beta loop
+    
+    outputXYAxes(iteratorY,2) = phi; % sets second column to be the phi values
+    iteratorY = iteratorY + 1;
+    
+end % ends phi loop
+
+% plot the contour map of phi vs beta with no lines between regions
+contour(outputXYAxes(:,1),outputXYAxes(:,2),contourMatrix,10000,'Fill','on','LineColor','flat');
+hold on
+% now plot the 25% survival line
+[contourLine, ~] = contour(outputXYAxes(:,1),outputXYAxes(:,2),contourMatrix,[25 25],'LineColor','k','LineWidth',4);
+
+% add a color bar and label it
+c = colorbar;
+caxis([0 100])
+c.Label.String = 'Percent Survival';
+
+% delete the first column of contourLine since it has weird values that we are ignoring
+contourLine(:,1) = [];
+
+% save contourLine to a matrix file to use in other programs
+save('ContourLine.mat','contourLine')
+
+% plot the three disease case points
+for i = [168 117 57]
+	plot(contourLine(1,i),contourLine(2,i),'h','MarkerSize',24,'MarkerEdgeColor','white','MarkerFaceColor','white')
+end
+
+% preserve correct plot aspect ratio
+axis image
+
+% label axes
+xlabel('\beta')
+ylabel('\phi')
+
+% set figure font size and axes line width
+set(gca, 'FontSize', 40, 'LineWidth', 2.5);
+
+% set the size of the saved figure
+set(gcf,'InvertHardcopy','on');
+set(gcf,'PaperUnits', 'inches');
+set(gcf,'PaperPosition', [0, 0, 20, 20]);
+
+% save the figure as a JPG with 450 dpi
+print(gcf,'25SurvivalContour','-djpeg','-r450')
---
+++ two-subpopulation/HibernaculumGrid.m
@@ -0,0 +1,167 @@
+% MATLAB code for J Duan, MM Malakhov, JJ Pellett, IS Phadke, J Barber, JC Blackwood. "Management Efficacy in a Spatially Dynamic Model of White-nose Syndrome"
+
+% Our multi-subpopulation code follows an object-oriented structure. This is the
+% class file for a grid of hibernacula/subpopulations, which provides a "blueprint"
+% for creating a grid of Hibernaculum objects and accounts for migration between them.
+
+% Instructions: DO NOT RUN THIS FILE -- this file is used by all other multi-subpopulation
+% program files.
+% This file does not require any manual changes, unless you want to change
+% the way that migration is handled.
+
+% Dependencies: Hibernaculum.m must be in the same directory
+
+% version 08/05/2019
+% Copyright (c) 2019 Mykhaylo M. Malakhov
+
+classdef HibernaculumGrid < handle
+
+% HibernaculumGrid is a subclass of handle, which means that all HibernaculumGrid
+% objects are handles; the upshot is that copying a HibernaculumGrid object
+% will only copy the handle, so all copies will refer to the same object;
+% this is necessary so we can create methods that modify properties without
+% having to return the object and reassign it to itself at every modification
+    
+    % declare the properties of HibernaculumGrid;
+    % every HibernaculumGrid object contains these two structures
+    properties
+        value % a Hibernaculum object
+        migrationMatrix % a matrix that stores proportions for migration
+    end
+    
+    % the methods of HibernaculumGrid: these are functions that act on the
+    % HibernaculumGrid object
+    methods
+        
+        % the constructor method that returns a HibernaculumGrid object
+        function this = HibernaculumGrid(m,n,beta,phi)
+            
+            % check to make sure that four arguments were submitted; for
+            % some reason MATLAB throws an error if this check is absent
+            if nargin == 4
+                this = HibernaculumGrid(m,n); % make HibernaculumGrid an m x n grid
+                
+                % go through the whole grid, creating a Hibernaculum object in each cell
+                for i = 1:m
+                    for j = 1:n
+                        this(i,j).value = Hibernaculum(beta,phi);
+                    end
+                end
+            end
+        end
+        
+        % this method sets up the migration matrix, which for convenience
+        % I place within the (1,1) HibernaculumGrid object;
+        % migrationMatrix is a huge m*n x m*n matrix because we need to
+        % account for migration from each subpopulation to every
+        % other subpopulation; every migration event (where immigration
+        % and emigration are two separate events) is represented
+        % by a cell in migrationMatrix
+        function ResetMigration(this,dispersal)
+            
+            % we can't access m,n from above, so we calclate them from the
+            % size of HibernaculumGrid
+            m = size(this,1);
+            n = size(this,2);
+            
+            % preallocate the size of migrationMatrix
+            this(1,1).migrationMatrix = zeros(m*n, m*n);
+            
+            for i = 1:m*n
+                for j = 1:m*n
+                    
+                    % the non-diagonal cells of migrationMatrix represent
+                    % the immigration events; we divide the
+                    % proportion of bats that leave each hibernaculum
+                    % by the total number of hibernacula that they will
+                    % move to, and place that value in each non-diagonal cell
+                    if i ~= j
+                        this(1,1).migrationMatrix(i,j) = dispersal / (m*n - 1);
+                    end
+                end
+            end
+            
+            % the diagonal cells of migrationMatrix represent the emigration
+            % events; we place the proportion of bats that are left
+            % in each hibernaculum in the diagonal cells
+            for k = 1:m*n
+                this(1,1).migrationMatrix(k,k) = 1 - sum(this(1,1).migrationMatrix(:,k));
+            end
+        end
+        
+        % this method applies migrationMatrix to the current population
+        function Migration(this)
+            
+            % we can't access m,n from above, so we calclate them from the
+            % size of HibernaculumGrid
+            m = size(this,1);
+            n = size(this,2);
+            
+            % this matrix temporarily stores fullPopulationMatrix from
+            % every Hibernaculum object together
+            tempPopMatrix = zeros(m*n,4);
+            
+            % copy the current population densities from fullPopulationMatrix
+            % from every Hibernaculum object into tempPopMatrix
+            i = 1;
+            for j = 1:m
+                for k = 1:n
+                    tempPopMatrix(i,:) = this(j,k).value.fullPopulationMatrix(end,1:4);
+                    i = i + 1;
+                end
+            end
+            
+            % the columns of tempPopMatrix are the four bat population state
+            % variables; multiply each one separately by migrationMatrix
+            for i = 1:4
+                tempPopMatrix(:,i) = this(1,1).migrationMatrix * tempPopMatrix(:,i);
+            end
+            
+            % make an additional row in fullPopulationMatrix for each
+            % Hibernaculum and place the "migrated" population values
+            % there, then place the unchanged P population in that row as well;
+            % this row will be deleted after running Hibernation in
+            % the Hibernaculum class
+            i = 1;
+            for q = 1:m
+                for w = 1:n
+                    this(q,w).value.fullPopulationMatrix(end+1,1:4) = tempPopMatrix(i,:);
+                    this(q,w).value.fullPopulationMatrix(end,5) = this(q,w).value.fullPopulationMatrix(end-1,5);
+                    i = i + 1;
+                end
+            end
+        end
+        
+        % this method simulates a full year of population dynamics
+        function FullYear(this,yearDays)
+            
+            % we can't access m,n from above, so we calclate them from the
+            % size of HibernaculumGrid
+            m = size(this,1);
+            n = size(this,2);
+            
+            % call the PreMigration method on each Hibernaculum object in
+            % HibernaculumGrid
+            for i = 1:m
+                for j = 1:n
+                    this(i,j).value.PreMigration(yearDays)
+                end
+            end
+            
+            % call the HibernaculumGrid Migration method; note that this
+            % method is only called once because it takes care of migration
+            % for the whole grid
+            this.Migration
+            
+            % now call the PostMigration method on each Hibernaculum object
+            % in HibernaculumGrid
+            for i = 1:m
+                for j = i:n
+                    this(i,j).value.PostMigration(yearDays)
+                end
+            end
+        end
+        
+    end
+    
+end
